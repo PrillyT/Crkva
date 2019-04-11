7377fc934e825a43f2a58d4b8159b8755fdf1df0
PrillyT <32259091+PrillyT@users.noreply.github.com>
2019-04-12 01:52:15 +0300

    Letter

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+
+    public GameObject letter;
+
+    public void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            letter.SetActive(false);
+        }
+    }
+}
